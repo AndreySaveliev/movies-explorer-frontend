9d9993ee044fe95e2390cf0d6e315f03ef9e6be5
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2022-12-11 21:43:51 +0300

    techs copm fix

--- src/components/Main/Techs/Techs.js
+++ src/components/Main/Techs/Techs.js
@@ -7,7 +7,7 @@ function Techs() {
         <h2 className="techs__title">Технологии</h2>
         <div className="techs__paragraph">
           <h3 className="techs__heading">7 технологий</h3>
-          <p className="techs__paragraph">
+          <p className="techs__text">
             На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.
           </p>
         </div>
