e96531c6233ccc77e05105dc13d366be5fcb196a
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2022-12-11 21:28:57 +0300

    portfolio copm changes

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -2,40 +2,67 @@ import React from 'react';
 
 function Portfolio() {
   return (
-    <div className="portfolio">
-      <div className="portfolio__wrapper">
+    <section className="portfolio">
+      <div className="portfolio-wrapper">
         <h2 className="portfolio__title">Портфолио</h2>
         <ul className="portfolio__links-list">
           <li className="portfolio__link-point">
             <a
               className="portfolio__link portfolio__link_static"
               href="https://github.com/AndreySaveliev/how-to-learn"
+              target="_blank"
+              rel="noreferrer"
             >
               Статичный сайт
             </a>
-            <a className='portfolio__link-symbol' href="https://github.com/AndreySaveliev/how-to-learn">↗</a>
+            <a
+              className="portfolio__link-symbol"
+              href="https://github.com/AndreySaveliev/how-to-learn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              ↗
+            </a>
           </li>
           <li className="portfolio__link-point portfolio__link-point_middle">
             <a
               className="portfolio__link portfolio__link_adaptiv"
               href="https://github.com/AndreySaveliev/russian-travel"
+              target="_blank"
+              rel="noreferrer"
             >
               Адаптивный сайт
             </a>
-            <a className='portfolio__link-symbol' href="https://github.com/AndreySaveliev/russian-travel">↗</a>
+            <a
+              className="portfolio__link-symbol"
+              href="https://github.com/AndreySaveliev/russian-travel"
+              target="_blank"
+              rel="noreferrer"
+            >
+              ↗
+            </a>
           </li>
           <li className="portfolio__link-point">
             <a
               className="portfolio__link portfolio__link_onepage"
               href="https://github.com/AndreySaveliev/react-mesto-api-full"
+              target="_blank"
+              rel="noreferrer"
             >
               Одностраничное приложение
             </a>
-            <a className='portfolio__link-symbol' href="https://github.com/AndreySaveliev/react-mesto-api-full">↗</a>
+            <a
+              className="portfolio__link-symbol"
+              href="https://github.com/AndreySaveliev/react-mesto-api-full"
+              target="_blank"
+              rel="noreferrer"
+            >
+              ↗
+            </a>
           </li>
         </ul>
       </div>
-    </div>
+    </section>
   );
 }
 
