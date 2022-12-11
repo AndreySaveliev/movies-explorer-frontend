25a6a9819d06e1a20ac979aaaf30649e944c6989
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2022-12-11 21:25:10 +0300

    BEM changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,34 +1,49 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import userimg from '../../images/back__COLOR_special-1.png'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Header({isLogged, isMain}) {
-
+function Header({ isLogged, isMain }) {
   return (
-    <div className={`header ${isMain && 'header__promo'}`}>
-      <div className='header__wrapper'>
-        <Link to='/' className='header__logo'></Link>
-        {isLogged ? 
-        <>
-          <input type='checkbox' className='header__checkbox'></input>
-          <label className='header__label'> </label>
-          <div className='header__navbar'>
-            <Link to='/' className='header__btn header__btn_main'>Главная</Link>
-            <Link to='/movies' className={` header__btn header__btn_film ${isLogged && 'header__btn_logged'}`}>Фильмы</Link>
-            <Link to='/saved-movies' className='header__btn_savfilm header__btn'>Сохраненные фильмы</Link>
-            <div className='header__user'>
-              <Link to='/profile' className='header__btn_user header__btn'>Аккаунт</Link>
-              <button className='header__userimg'  alt='значок юзера'></button>
+    <header className={`header ${isMain && 'header-promo'}`}>
+      <div className="header-wrapper">
+        <Link to="/" className="header-wrapper__logo"></Link>
+        {isLogged ? (
+          <>
+            <input type="checkbox" className="header__checkbox"></input>
+            <label className="header__label"> </label>
+            <div className="header-navbar">
+              <Link to="/" className="header-navbar__btn header-navbar__btn_main">
+                Главная
+              </Link>
+              <Link
+                to="/movies"
+                className={` header-navbar__btn header-navbar__btn_film ${isLogged && 'header-navbar__btn_logged'}`}
+              >
+                Фильмы
+              </Link>
+              <Link to="/saved-movies" className="header-navbar__btn_savfilm header-navbar__btn">
+                Сохраненные фильмы
+              </Link>
+              <div className="header-navbar__user">
+                <Link to="/profile" className="header-navbar__btn_user header-navbar__btn">
+                  Аккаунт
+                </Link>
+                <button className="header-navbar__userimg" alt="значок юзера"></button>
+              </div>
             </div>
-          </div> 
-        </>:
-        <div>
-          <Link to='/signup'className='header__btn_signup header__btn'>Регистрация</Link>
-          <Link to='/signin' className='header__btn_signin header__btn'>Войти</Link>
-        </div>}
+          </>
+        ) : (
+          <div>
+            <Link to="/signup" className="header__btn_signup header-navbar__btn">
+              Регистрация
+            </Link>
+            <Link to="/signin" className="header__btn_signin header-navbar__btn">
+              Войти
+            </Link>
+          </div>
+        )}
       </div>
-    </div>
-  )
+    </header>
+  );
 }
 
-export default Header
+export default Header;
