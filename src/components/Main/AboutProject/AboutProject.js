80bafa3baaa1364a2f6f2220f8c8119363d3fd85
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2022-12-11 21:28:32 +0300

    aboutproj comp cahnges

--- src/components/Main/AboutProject/AboutProject.js
+++ src/components/Main/AboutProject/AboutProject.js
@@ -1,33 +1,37 @@
-import React from 'react'
+import React from 'react';
 
 function AboutProject() {
   return (
-    <div className='about' id='about'>
-      <div className='about__wrapper'>
-        <h2 className='about__title'>О Проекте</h2>
-        <div className='about__text'>
-          <div className='text__paragraph'>
-            <h3 className='paragraph__title'>Дипломный проект включал 5 этапов</h3>
-            <p className='paragraph__text'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+    <section className="about" id="about">
+      <div className="about-wrapper">
+        <h2 className="about__title">О Проекте</h2>
+        <div className="about__text">
+          <div className="about__paragraph">
+            <h3 className="about__title">Дипломный проект включал 5 этапов</h3>
+            <p className="about__item">
+              Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.
+            </p>
           </div>
-          <div className='text__paragraph'>
-            <h3 className='paragraph__title'>На выполнение диплома ушло 5 недель</h3>
-            <p className='paragraph__text'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+          <div className="about__paragraph">
+            <h3 className="about__title">На выполнение диплома ушло 5 недель</h3>
+            <p className="about__item">
+              У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.
+            </p>
           </div>
         </div>
-        <div className='about__progress'>
-          <div className='progress__cell'>
-            <p className='progress__cell_back'>1 неделя</p>
-            <p className='progress__cell_front'>4 недели</p>
+        <div className="about__progress">
+          <div className="about__cell">
+            <p className="about__cell_back">1 неделя</p>
+            <p className="about__cell_front">4 недели</p>
           </div>
-          <div className='progress__name'>
-            <p className='progress__name_back'>Back-end</p>
-            <p className='progress__name_front'>Front-end</p>
+          <div className="about__name">
+            <p className="about__name_back">Back-end</p>
+            <p className="about__name_front">Front-end</p>
           </div>
         </div>
       </div>
-    </div>
-  )
+    </section>
+  );
 }
 
-export default AboutProject
+export default AboutProject;
