660c16a683182f24af204e4a3e6aae305822a8ae
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2022-12-11 21:29:44 +0300

    notfound copm changes

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -6,11 +6,11 @@ function NotFoundPage() {
   const navigate = useNavigate()
 
   return (
-    <div className='notfound'>
+    <section className='notfound'>
       <h1 className='notfound__title'>404</h1>
       <p className='notfound__text'>Страница не найдена</p>
       <button onClick={() => navigate(-1)} className='notfound__btn'>Назад</button>
-    </div>
+    </section>
   )
 }
 
