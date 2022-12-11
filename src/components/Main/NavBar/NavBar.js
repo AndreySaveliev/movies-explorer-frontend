44028ab804873db63e3563c0a9dc7cb53dcb43cf
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2022-12-11 21:28:48 +0300

    navbar copm chamges

--- src/components/Main/NavBar/NavBar.js
+++ src/components/Main/NavBar/NavBar.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 function NavBar() {
   return (
-    <div className='navbar'>
+    <section className='navbar'>
       <ul className='navbar__links'>
         <li className='navbar__link'><a className='navbar__link' href='#about'>О проекте</a></li>
         <li className='navbar__link'><a className='navbar__link' href='#techs'>Технологии</a></li>
         <li className='navbar__link'><a className='navbar__link' href='#aboutme'>Студент</a></li> 
       </ul>
-    </div>
+    </section>
   )
 }
 
