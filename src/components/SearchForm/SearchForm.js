63d5365796746011e088620c56afba6a054fae0c
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2022-12-11 21:30:11 +0300

    searchfrom copm changes

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,20 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 function SearchForm() {
-
   return (
-    <div className='searchform'>
-      <div className='searchform__form'>
-        <input className='form__input' placeholder='Фильм'></input>
-        <button className='form__btn'></button>
-        <div className='searchform__toggle'>
-          <input type='checkbox' className='form__checkbox'></input>
-          <label className='form__switch'></label>
-          <p className='form__text'>Короткометражки</p>
-        </div> 
-      </div>
-    </div>
-  )
+    <section className="searchform">
+      <form className="searchform__form" noValidate>
+        <input className="searchform__input" placeholder="Фильм" required></input>
+        <button className="searchform__btn"></button>
+        <div className="searchform__toggle">
+          <input type="checkbox" className="searchform__checkbox"></input>
+          <label className="searchform__switch"></label>
+          <p className="searchform__text">Короткометражки</p>
+        </div>
+      </form>
+    </section>
+  );
 }
 
-export default SearchForm
+export default SearchForm;
