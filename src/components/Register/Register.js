b074779c7d7071428b0e84108b9ef18c926611d6
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2022-12-11 21:29:53 +0300

    register copm changes

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,27 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from '../../images/logo.svg'
 function Register() {
   return (
-    <div className='register'>
+    <section className='register'>
       <div className='register__header'>
         <Link to='/' className='register__header-logo'></Link>
         <h1 className='register__header-title'>Добро пожаловать!</h1>
-        <form className='register__form'>
+      </div>
+      <form className='register__form'>
           <label className='register__form-label register__form-label_name'>Имя</label>
-          <input className='register__form-input register__form-input_name' value={'andrey'}></input>
+          <input className='register__form-input register__form-input_name' value={'andrey'} required></input>
           <label className='register__form-label register__form-label_email'>Email</label>
-          <input className='register__form-input register__form-input_email' value={'test@email'}></input>
+          <input className='register__form-input register__form-input_email' value={'test@email'} required></input>
           <label className='register__form-label register__form-label_password'>Пароль</label>
-          <input className='register__form-input register__form-input_password' type='password'></input>
+          <input className='register__form-input register__form-input_password' type='password' required></input>
         </form>
         <button className='register__btn register__btn_signup'>Зарегистрироваться</button>
         <div className='register__signin'>
           <p className='signin__text'>Уже зарегистрированы?</p>
           <Link to='/signin' className='register__btn register__btn_signin'>Войты</Link>
         </div>
-      </div>
-    </div>
+    </section>
   )
 }
 
