88996073469a45dc652f972a21fe458069f64706
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2022-12-31 02:35:40 +0300

    поменял редирект
    если пользователь не залогинен то его перенаправит на главную станицу, а не на авторизацию

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({children, isLogged}) => {
-  return isLogged ? children : <Navigate to='/signin' />
+  return isLogged ? children : <Navigate to='/' />
 }
 
 export default ProtectedRoute
