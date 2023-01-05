caf9b3cc621b7786d7216c482060b959b86fb84c
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2023-01-05 14:36:17 +0300

    popup fix

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,15 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-export default function Popup({ isPopupClosed, setIsPopupClosed, popupMessage }) {
-  const handlePopupClose = () => {
-    setIsPopupClosed(true);
-  };
+export default function Popup({ isPopupClosed, setIsPopupClosed, popupMessage, popupStatus}) {
 
   return (
-    <div className={`popup ${isPopupClosed && 'popup__hidden'}`}>
+    <div className={`popup ${popupStatus ? 'popup_success' : 'popup_fail'} ${isPopupClosed && 'popup__hidden'}`}>
       <p className="popup__text">{popupMessage}</p>
       {/* <p className='popup__text'>rf;tncz xnj nj gjikj yt nfr. jmhfnbntcm d cke,;e gjllth;rb</p> */}
-      <button className="popup__button" onClick={handlePopupClose}></button>
     </div>
   );
 }
